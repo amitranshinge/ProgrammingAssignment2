d2188643d8ba7a4857234b41fbedbafab66344cb
Amit Ranshinge <amitranshinge@gmail.com>
2014-10-24 02:18:00 +0530

    Submission for Assignment 2.

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,12 @@
 ## 2 functions have been defined makeCacheMatrix() and cacheSolve().
 ## These will be used to compute and cache the inverse of a given invertible matrix
 ## as required in the assignement.
+## (For simplicity I have used a code which is similar to the example given with the assignment)
 
-## The first function is called make CacheMatrix(). This function takes as an invertible
-## matrix. It will create a list of functions that will :
+## The first function is called makeCacheMatrix(). This function takes as an input, a square invertible
+## matrix. For example :
+## test_1 <- makeCacheMatrix(matrix(c(1,2,3,4), nrow=2 ,ncol=2))
+## This function will create a list of functions that will perform the following tasks:
 ## a) set the value of the matrix - set()
 ## b) get the value of the matrix - get()
 ## c) set the value of the inverse of the matrix - setinv()
@@ -27,7 +30,7 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Inverse is calculated and stored in the cache memory.
 ## If inverse is already calculated then it is retrieved from cache memory without
 ## having to recalculate the inverse, thereby saving precious resources.
-## Variable "m" will store the value of the inverse of the 
+## Variable "m" will store the value of the inverse of the matrix and will be returned at the end.
 
 cacheSolve <- function(x, ...) {
   m <- x$getinv()
